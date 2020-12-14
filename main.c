6c3f65e49148ad1464ac06868d6c1ef87edbed8c
Ana Juncosa montes <ajuncosa@c1r5s6.42madrid.com>
2020-12-14 14:44:18 +0100

    add: take path and user from envp & add: prompt with username

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ajuncosa <ajuncosa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/11 13:36:03 by ajuncosa          #+#    #+#             */
-/*   Updated: 2020/12/11 14:33:39 by ajuncosa         ###   ########.fr       */
+/*   Updated: 2020/12/14 13:08:51 by ajuncosa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,34 @@ int	main(int argc, char **argv, char **envp)
 {
 	int		i;
 	char	str[1000];
+	char	*path;
+	char	*user_line;
+	char	*user;
+	size_t	user_len;
 
+	path = NULL;
+	user = NULL;
+	i = 0;
+	while (envp[i])
+	{
+		if (!ft_strncmp(envp[i], "PATH=", 5))		// to do: take env variables :D
+			path = ft_strdup(envp[i]);
+		else if(!ft_strncmp(envp[i], "USER=", 5))
+			user_line = ft_strdup(envp[i]);
+		i++;
+	}
+	user = ft_strtrim(user_line, "USER=");
+	free(user_line);
+	user_len = ft_strlen(user);
 	while (1)
 	{
-		i = 0;
-		/*while (envp[i])
-		{
-			if (!ft_strncmp(envp[i], "USER=", 5))	// to do: take env variables :D	
-				
-		}*/
-
+		write(1, user, user_len);
+		write(1, "> ", 2);
 		read(0, str, 999);
-		if (!ft_strncmp(str, "exit", 4)) 			// to do: empty line after command :D
+		if (!ft_strncmp(str, "exit", 4)) 				// to do: empty line after command :D
 			exit(0);
 	}
+	free(path);
+	free(user);
 	return (0);
 }
