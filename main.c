d7412a4c8385c911f68b3dd3ced5782ee9936af7
Ana Juncosa <ajm.zetazix@gmail.com>
2020-12-16 14:34:14 +0100

    fixed echo's spaces management and unquoted words

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ void	echo(char *str)
 {
 	int	n;
 	int	i;
-	int	end_quote;
+	int	end;
 
 	n = 0;
 	i = 0;
@@ -27,34 +27,44 @@ void	echo(char *str)
 		n = 1;
 		i += 2;
 	}
+	while (str[i] == ' ')												// para saltarse los espacios extra después de echo o después de -n
+		i++;
 	while (str[i] != '\n')
 	{
-		while (str[i] == ' ')
-			i++;
 		if (str[i] == '"')
 		{
 			i++;
-			end_quote = i;
-			while (str[end_quote] != '"' && str[end_quote] != '\n')
-				end_quote++;
-			if (str[end_quote] != '"')
+			end = i;
+			while (str[end] != '"' && str[end] != '\n')
+				end++;
+			if (str[end] != '"')
 				write(1, "COMILLA ABIERTA\n", 16);						// fix: qué pasa si se queda la comilla abierta!?
-			write(1, &str[i], end_quote - i);
-			i = end_quote + 1;
+			write(1, &str[i], end - i);
+			i = end + 1;
 		}
 		else if (str[i] == '\'')
 		{
 			i++;
-			end_quote = i;
-			while (str[end_quote] != '\'' && str[end_quote] != '\n')
-				end_quote++;
-			if (str[end_quote] != '\'')
+			end = i;
+			while (str[end] != '\'' && str[end] != '\n')
+				end++;
+			if (str[end] != '\'')
 				write(1, "COMILLA ABIERTA\n", 16);
-			write(1, &str[i], end_quote - i);
-			i = end_quote + 1;
+			write(1, &str[i], end - i);
+			i = end + 1;
 		}
 		else
-			write(1, &str[i], ft_strlen(&str[i]) - 1);
+		{
+			end = i;													//para imprimir por palabras con un solo espacio, y teniendo en cuenta que pueden venir strings con comillas después
+			while (str[end] != ' ' && str[end] != '"' && str[end] != '\'' && str[end] != '\n')
+				end++;
+			write(1, &str[i], end - i);
+			i = end;
+		}
+		while (str[i] == ' ')											// he movido esto al final del bucle para que solo haga lo de poner un espacio (ver siguiente comentario) cuando es entre palabras, no al principio de la primera palabra (xq encuentra el espacio de "echo ")
+			i++;
+		if (str[i - 1] == ' ')											// esto es para que cuando hay varias palabras separadas por uno o varios espacios, que ponga sólo uno
+			write(1, " ", 1);
 	}
 	if (n == 0)
 		write(1, "\n", 1);
@@ -93,8 +103,11 @@ int		main(int argc, char **argv, char **envp)
 		read(0, str, 1023);
 		if (!ft_strncmp(str, "echo ", 5))
 			echo(&str[5]);
-		if (!ft_strncmp(str, "exit", 4)) 				// to do: check if line is empty after command :D
+		else if (!ft_strncmp(str, "exit", 4)) 				// to do: check if line is empty after command :D
 			exit(0);
+		else
+			write(1, "Command not found\n", 18);
+		
 		ft_bzero(str, 1023);
 	}
 
