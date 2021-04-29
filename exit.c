27778264e0698a2d7b6d00fcee364b861cb78268
cruiz-de <charlierdlp@gmail.com>
2021-04-29 14:08:20 +0200

    Add: norme

--- exit.c
+++ exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ajuncosa <ajuncosa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cruiz-de <cruiz-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 12:47:45 by ajuncosa          #+#    #+#             */
-/*   Updated: 2021/04/28 18:08:13 by ajuncosa         ###   ########.fr       */
+/*   Updated: 2021/04/29 14:06:24 by cruiz-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,67 @@
 
 void	del_lst(void *env)
 {
-	free(((t_env*)env)->id);
-	free(((t_env*)env)->value);
-	free((t_env*)env);
+	free(((t_env *)env)->id);
+	free(((t_env *)env)->value);
+	free((t_env *)env);
 }
 
 void	del_lst_cmd(void *cmd)
 {
-	int j;
+	int	j;
 
-	if (((t_cmd*)cmd)->cmd)
-		free(((t_cmd*)cmd)->cmd);
-	if (((t_cmd*)cmd)->args_str)
+	if (((t_cmd *)cmd)->cmd)
+		free(((t_cmd *)cmd)->cmd);
+	if (((t_cmd *)cmd)->args_str)
 	{
 		j = 0;
-		while (j < ((t_cmd*)cmd)->n_args)
+		while (j < ((t_cmd *)cmd)->n_args)
 		{
-			free(((t_cmd*)cmd)->args_str[j]);
+			free(((t_cmd *)cmd)->args_str[j]);
 			j++;
 		}
-		free(((t_cmd*)cmd)->args_str);
+		free(((t_cmd *)cmd)->args_str);
 	}
-
-	if (((t_cmd*)cmd)->args)
+	if (((t_cmd *)cmd)->args)
 	{
 		j = 0;
-		while (j < ((t_cmd*)cmd)->n_args)
+		while (j < ((t_cmd *)cmd)->n_args)
 		{
-			free(((t_cmd*)cmd)->args[j]);
+			free(((t_cmd *)cmd)->args[j]);
 			j++;
 		}
-		free(((t_cmd*)cmd)->args);
+		free(((t_cmd *)cmd)->args);
 	}
-	free((t_cmd*)cmd);
+	free((t_cmd *)cmd);
+}
+
+void	check_args(t_data *data, t_cmd *com)
+{
+	int	i;
+
+	i = 0;
+	data->ret = ft_atoi(com->args_str[0]);
+	if (data->ret >= 256)
+		data->ret -= 256;
+	else if (data->ret < 0)
+		data->ret += 256;
+	if (com->args_str[0][0] == '-')
+	{
+		if (com->args_str[0][1] == '-' && com->args_str[0][2] == '\0')
+			i = 2;
+		else if (ft_isdigit(com->args_str[0][1]))
+			i = 1;
+	}
+	while (com->args_str[0][i])
+	{
+		if (!ft_isdigit(com->args_str[0][i]))
+		{
+			printf("exit: %s: numeric argument required\n", com->args_str[0]);
+			data->ret = 255;
+			break ;
+		}
+		i++;
+	}
 }
 
 void	ft_exit(t_data *data, t_cmd *com)
@@ -57,7 +85,7 @@ void	ft_exit(t_data *data, t_cmd *com)
 	if (com)
 	{
 		if (!ft_strcmp(com->cmd, "exit"))
-		{
+		{	
 			if (com->n_args > 1)
 			{
 				printf("exit: too many arguments\n");
@@ -65,34 +93,9 @@ void	ft_exit(t_data *data, t_cmd *com)
 				return ;
 			}
 			if (com->n_args > 0)
-			{
-				i = 0;
-				data->ret = ft_atoi(com->args_str[0]);
-				if (data->ret >= 256)
-					data->ret -= 256;
-				else if (data->ret < 0)
-					data->ret += 256;
-				if (com->args_str[0][0] == '-')
-				{
-					if (com->args_str[0][1] == '-' && com->args_str[0][2] == '\0')
-						i = 2;
-					else if (ft_isdigit(com->args_str[0][1]))
-						i = 1;
-				}
-				while(com->args_str[0][i])
-				{
-					if (!ft_isdigit(com->args_str[0][i]))
-					{
-						printf("exit: %s: numeric argument required\n", com->args_str[0]);
-						data->ret = 255;
-						break ;
-					}
-					i++;
-				}
-			}
+				check_args(data, com);
 		}
 	}
-
 	if (g_user)
 		free(g_user);
 	if (data->line)
