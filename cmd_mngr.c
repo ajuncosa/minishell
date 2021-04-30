cefc9fe9c9ae7c7f2bcc40b284564da26547293e
ajuncosa <ajm.zetazix@gmail.com>
2021-04-30 13:31:23 +0200

    add: some norme cmd_mngr.c

--- cmd_mngr.c
+++ cmd_mngr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cmd_mngr.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cruiz-de <cruiz-de@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ajuncosa <ajuncosa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 12:05:03 by cruiz-de          #+#    #+#             */
-/*   Updated: 2021/04/29 17:45:11 by cruiz-de         ###   ########.fr       */
+/*   Updated: 2021/04/30 13:27:12 by ajuncosa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,92 +35,92 @@ void	cmd_caller(t_cmd *com, t_data *data, char **envp)
 		ft_cmd(com, envp, data);
 }
 
-void	cmd_manager(t_data *data, char **envp)
+void	redirs_and_exec(t_data *data, t_cmd *com, char **envp)
+{
+	if (check_if_redir(com))
+		redir_manager(com, data, envp);
+	else
+	{
+		if (!create_args_str(com))
+			ft_exit(data, com);
+		cmd_caller(com, data, envp);
+	}
+}
+
+void	handle_pipe_input(t_data *data, char **envp, t_cmd *com, int *fd_read)
 {
 	int		fd[2];
-	int		fd_read;
-	t_list	*lst;
 	char	*sterr;
 	int		status;
+
+	pipe(fd);
+	g_pid = fork();
+	if (g_pid == 0)
+	{
+		if (*fd_read)
+		{
+			dup2(*fd_read, STDIN_FILENO);
+			close(*fd_read);
+		}
+		close(fd[0]);
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+		redirs_and_exec(data, com, envp);
+		exit(data->ret);
+	}
+	else if (g_pid < 0)
+	{
+		sterr = strerror(errno);
+		error_msn(NULL, NULL, sterr);
+	}
+	wait(&status);
+	data->ret = WEXITSTATUS(status);
+	if (*fd_read)
+		close(*fd_read);
+	*fd_read = fd[0];
+	close(fd[1]);
+}
+
+void	handle_pipe_output(t_data *data, char **envp, t_cmd *com, int *fd_read)
+{
+	char	*sterr;
+	int		status;
+
+	g_pid = fork();
+	if (g_pid == 0)
+	{
+		dup2(*fd_read, STDIN_FILENO);
+		close(*fd_read);
+		redirs_and_exec(data, com, envp);
+		exit(data->ret);
+	}
+	else if (g_pid < 0)
+	{
+		sterr = strerror(errno);
+		error_msn(NULL, NULL, sterr);
+	}
+	wait(&status);
+	data->ret = WEXITSTATUS(status);
+	close(*fd_read);
+	*fd_read = 0;
+}
+
+void	cmd_manager(t_data *data, char **envp)
+{
+	int		fd_read;
+	t_list	*lst;
 
 	lst = data->cmd_head;
 	fd_read = 0;
 	while (lst)
 	{
-		if (((t_cmd *)lst->content)->sep_0 != '|' && ((t_cmd *)lst->content)->sep_1 != '|')
-		{
-			if (check_if_redir(((t_cmd *)lst->content)))
-				redir_manager(((t_cmd *)lst->content), data, envp);
-			else
-			{
-				if (!create_args_str((t_cmd *)lst->content))
-					ft_exit(data, (t_cmd *)lst->content);
-				cmd_caller(((t_cmd *)lst->content), data, envp);
-			}
-		}
+		if (((t_cmd *)lst->content)->sep_0 != '|'
+			&& ((t_cmd *)lst->content)->sep_1 != '|')
+			redirs_and_exec(data, (t_cmd *)lst->content, envp);
 		if (((t_cmd *)lst->content)->sep_1 == '|')
-		{
-			pipe(fd);
-			g_pid = fork();
-			if (g_pid == 0)
-			{
-				if (fd_read)
-				{
-					dup2(fd_read, STDIN_FILENO);
-					close(fd_read);
-				}
-				close(fd[0]);
-				dup2(fd[1], STDOUT_FILENO);
-				close(fd[1]);
-				if (check_if_redir(((t_cmd *)lst->content)))
-					redir_manager(((t_cmd *)lst->content), data, envp);
-				else
-				{
-					if (!create_args_str((t_cmd *)lst->content))
-						ft_exit(data, (t_cmd *)lst->content);
-					cmd_caller(((t_cmd *)lst->content), data, envp);
-				}
-				exit(data->ret);
-			}
-			else if (g_pid < 0)
-			{
-				sterr = strerror(errno);
-				error_msn(NULL, NULL, sterr);
-			}
-			wait(&status);
-			data->ret = WEXITSTATUS(status);
-			if (fd_read)
-				close(fd_read);
-			fd_read = fd[0];
-			close(fd[1]);
-		}
+			handle_pipe_input(data, envp, (t_cmd *)lst->content, &fd_read);
 		else if (((t_cmd *)lst->content)->sep_0 == '|')
-		{
-			g_pid = fork();
-			if (g_pid == 0)
-			{
-				dup2(fd_read, STDIN_FILENO);
-				close(fd_read);
-				if (check_if_redir(((t_cmd *)lst->content)))
-					redir_manager(((t_cmd *)lst->content), data, envp);
-				else
-				{
-					if (!create_args_str((t_cmd *)lst->content))
-						ft_exit(data, (t_cmd *)lst->content);
-					cmd_caller(((t_cmd *)lst->content), data, envp);
-				}
-				exit(data->ret);
-			}
-			else if (g_pid < 0)
-			{
-				sterr = strerror(errno);
-				error_msn(NULL, NULL, sterr);
-			}
-			wait(&status);
-			data->ret = WEXITSTATUS(status);
-			close(fd_read);
-			fd_read = 0;
-		}
+			handle_pipe_output(data, envp, (t_cmd *)lst->content, &fd_read);
 		lst = lst->next;
 	}
 }
